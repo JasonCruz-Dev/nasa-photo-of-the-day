1aa4cb31aa450dcecba1db008f9b59affd7dbfab
Jason Cruz <badmonkey.dev@gmail.com>
2019-11-21 20:35:15 -0600

    edited app.js to suit needs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./App.css";
+import NasaApi from "./components/NasaApi";
 
 function App() {
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+    <div className='App'>
+      <NasaApi />
     </div>
   );
 }
